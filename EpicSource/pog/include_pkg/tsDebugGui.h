14c7d5b6d19c3085afdc2244038b8ff5e02bd299
jon <jon@374041d0-98b9-bf4f-901b-463804fe4fe0>
2007-01-19 21:27:08 +0000

    debug gui seperated from debug flags to get around linker issue.

---
+++ EpicSource/pog/include_pkg/tsDebugGui.h
@@ -0,0 +1,28 @@
+//
+// (c) 1998-9 Particle Systems Ltd. All Rights Reserved
+//
+// tsDebugGui.h
+//
+// Compiler-generated export header for package "tsDebugGui"
+//
+// Revision control information:
+//
+// $Header: /flux/src/compiler/TypeChecker.cpp 31    27/03/01 14:31 Will $
+//
+
+// Dependencies ////////////////////////////////////////////////////////////////
+
+// Enumerated types ////////////////////////////////////////////////////////////
+
+// Functions ///////////////////////////////////////////////////////////////////
+
+prototype tsDebugGui.StartScreen();
+
+prototype tsDebugGui.ExitScreen();
+
+prototype tsDebugGui.DisplayWindow();
+
+prototype tsDebugGui.OnDebugPress();
+
+// EOF /////////////////////////////////////////////////////////////////////////
+
