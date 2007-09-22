4d1ad8375b5f187fd655a0bbdfceeff3914f639e
james <james@374041d0-98b9-bf4f-901b-463804fe4fe0>
2007-09-22 05:39:13 +0000

    missing file tsShipCreationEnum.h added back in. Thank goodness Jon had a backup.

---
+++ EpicSource/pog/include/tsShipCreationEnum.h
@@ -0,0 +1,53 @@
+enum etsShipType
+{
+   EST_Invalid,
+	EST_Gunstar,
+
+	EST_Drone,
+	EST_Waldo,
+	EST_CommandSection,
+	EST_Utility,
+	EST_Passenger,
+	EST_Fighter,
+	EST_Tug,
+	EST_Patcom,
+	EST_Interceptor,
+	EST_Corvette,
+	EST_Freighter,
+	EST_Destroyer,
+	EST_Cruiser,
+	EST_Carrier,  
+
+	EST_FreightLight, 
+   EST_FreightHeavy, 
+   EST_FreightAll, 
+   EST_FreightSuperHeavy, 
+	EST_FreightCourier, 
+
+   EST_CombatFighter, 
+   EST_CombatLight, 
+   EST_CombatHeavy,
+	EST_CombatCaptial, 
+   EST_CombatAll, 
+
+   EST_Medical, 
+   EST_Interface, 
+   EST_PassengerLight, 
+	EST_PassengerHeavy, 
+   EST_PassengerAll, 
+
+   EST_MiningLight, 
+   EST_MiningHeavy, 
+   EST_MiningAll,
+
+	EST_TankerLight, 
+   EST_TankerHeavy, 
+   EST_TankerAll, 
+
+   EST_GeneralPurpose, 
+   EST_Flitter, 
+   EST_Recovery,
+	EST_DemoShips, 
+   EST_Taxi,
+   EST_Max
+};
