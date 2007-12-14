09ddf67aa9f6219fb65348eb19b01251d133e1f3
jon <jon@374041d0-98b9-bf4f-901b-463804fe4fe0>
2007-12-14 02:13:18 +0000

    Added Subsystem description window to the equipment trade screens.

---
+++ EpicSource/pog/include_pkg/tsDescribeINI.h
@@ -0,0 +1,24 @@
+//
+// (c) 1998-9 Particle Systems Ltd. All Rights Reserved
+//
+// tsDescribeINI.h
+//
+// Compiler-generated export header for package "tsDescribeINI"
+//
+// Revision control information:
+//
+// $Header: /flux/src/compiler/TypeChecker.cpp 31    27/03/01 14:31 Will $
+//
+
+// Dependencies ////////////////////////////////////////////////////////////////
+
+// Enumerated types ////////////////////////////////////////////////////////////
+
+// Functions ///////////////////////////////////////////////////////////////////
+
+prototype string tsDescribeINI.Subsystem( string subsim_template );
+
+prototype string tsDescribeINI.Projectile( string weapon_template );
+
+// EOF /////////////////////////////////////////////////////////////////////////
+
