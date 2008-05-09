0c149fb10c183394357d3a736377bfa3629eed7f
james <james@374041d0-98b9-bf4f-901b-463804fe4fe0>
2008-05-09 10:47:58 +0000

    Added code to support station watcher task. The is linked into tsLocationInit.Arrive and .Leave functions. The actual implementation is in tsStationAi.pog
    Stations have also been made destroyable as an experiment.

---
+++ EpicSource/pog/include_pkg/tsStationAi.h
@@ -0,0 +1,26 @@
+//
+// (c) 1998-9 Particle Systems Ltd. All Rights Reserved
+//
+// tsStationAi.h
+//
+// Compiler-generated export header for package "tsStationAi"
+//
+// Revision control information:
+//
+// $Header: /flux/src/compiler/TypeChecker.cpp 31    27/03/01 14:31 Will $
+//
+
+// Dependencies ////////////////////////////////////////////////////////////////
+
+// Enumerated types ////////////////////////////////////////////////////////////
+
+// Functions ///////////////////////////////////////////////////////////////////
+
+prototype tsStationAi.Reaction();
+
+prototype bool tsStationAi.Start( hhabitat station );
+
+prototype bool tsStationAi.End( hhabitat station );
+
+// EOF /////////////////////////////////////////////////////////////////////////
+
