36309e94be6c776605e1c6a2965132b4a4645954
james <james@374041d0-98b9-bf4f-901b-463804fe4fe0>
2008-05-30 22:17:53 +0000

    Interim work in developing a Trade Operation.

---
+++ EpicSource/pog/include_pkg/tsFoTrade.h
@@ -0,0 +1,28 @@
+//
+// (c) 1998-9 Particle Systems Ltd. All Rights Reserved
+//
+// tsFoTrade.h
+//
+// Compiler-generated export header for package "tsFoTrade"
+//
+// Revision control information:
+//
+// $Header: /flux/src/compiler/TypeChecker.cpp 31    27/03/01 14:31 Will $
+//
+
+// Dependencies ////////////////////////////////////////////////////////////////
+
+// Enumerated types ////////////////////////////////////////////////////////////
+
+// Functions ///////////////////////////////////////////////////////////////////
+
+prototype tsFoTrade.New( hisim operation );
+
+prototype tsFoTrade.Save( hisim operation );
+
+prototype tsFoTrade.Restore( hisim operation );
+
+prototype tsFoTrade.Update( hisim operation );
+
+// EOF /////////////////////////////////////////////////////////////////////////
+
