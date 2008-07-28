e1b490e0583db73ac3ebbd1adea5041b6c8efa63
james <james@374041d0-98b9-bf4f-901b-463804fe4fe0>
2008-07-28 07:53:10 +0000

    New ship escort utility package. Provides utility functions to create escort fleets in a consistent manner, reducing redundant code.

---
+++ EpicSource/pog/include_pkg/tsShipEscort.h
@@ -0,0 +1,22 @@
+//
+// (c) 1998-9 Particle Systems Ltd. All Rights Reserved
+//
+// tsShipCreation.h
+//
+// Compiler-generated export header for package "tsShipCreation"
+//
+// Revision control information:
+//
+// $Header: /flux/src/compiler/TypeChecker.cpp 31    27/03/01 14:31 Will $
+//
+
+// Dependencies ////////////////////////////////////////////////////////////////
+
+// Enumerated types ////////////////////////////////////////////////////////////
+
+// Functions ///////////////////////////////////////////////////////////////////
+
+prototype tsShipEscort.CreateStandardEscort( hisim operation, hship leader, hgroup cull_group, string ship_name_group, string ship_type );
+
+// EOF /////////////////////////////////////////////////////////////////////////
+
