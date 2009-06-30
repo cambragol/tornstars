845d6dd1ae9a144612c7894acfcbc484be982a74
james <james@374041d0-98b9-bf4f-901b-463804fe4fe0>
2009-06-30 09:12:29 +0000

    First cut of Free Trader Operation. Free Trader operation is a trader version of the Merc op. Provides hireable ships but in a more extensible and configurable model.

---
+++ EpicSource/pog/include_pkg/tsFoFreeTrader.h
@@ -0,0 +1,30 @@
+//
+// (c) 1998-9 Particle Systems Ltd. All Rights Reserved
+//
+// tsFoFreeTrader.h
+//
+// Compiler-generated export header for package "tsFoFreeTrader"
+//
+// Revision control information:
+//
+// $Header: /flux/src/compiler/TypeChecker.cpp 31    27/03/01 14:31 Will $
+//
+
+// Dependencies ////////////////////////////////////////////////////////////////
+
+// Enumerated types ////////////////////////////////////////////////////////////
+
+// Functions ///////////////////////////////////////////////////////////////////
+
+prototype tsFoFreeTrader.New( hisim operation );
+
+prototype tsFoFreeTrader.Save( hisim operation );
+
+prototype tsFoFreeTrader.Restore( hisim operation );
+
+prototype tsFoFreeTrader.Update( hisim operation );
+
+prototype tsFoFreeTrader.PlayerNear( hisim operation );
+
+// EOF /////////////////////////////////////////////////////////////////////////
+
---
+++ EpicSource/pog/include_pkg/tsPcFreeTrader.h
@@ -0,0 +1,24 @@
+//
+// (c) 1998-9 Particle Systems Ltd. All Rights Reserved
+//
+// tsPcFreeTrader.h
+//
+// Compiler-generated export header for package "tsPcFreeTrader"
+//
+// Revision control information:
+//
+// $Header: /flux/src/compiler/TypeChecker.cpp 31    27/03/01 14:31 Will $
+//
+
+// Dependencies ////////////////////////////////////////////////////////////////
+
+// Enumerated types ////////////////////////////////////////////////////////////
+
+// Functions ///////////////////////////////////////////////////////////////////
+
+prototype hsim tsPcFreeTrader.PogCreate( string template_name, string object_name );
+
+prototype PogStart tsPcFreeTrader.PogStart( hsim pog_object );
+
+// EOF /////////////////////////////////////////////////////////////////////////
+
